5145a6e080ad579173d5bbccfec7cb26985768c2
fitpie <yf@fitpie.com>
2023-01-09 18:50:28 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  base: '/site-appxyz/',
+  title: 'Appxyz',
+  themeConfig: {
+    footer: {
+      copyright:
+        'Copyright © 2023 Appxyz. All rights reserved.'
+    }
+  }
+}
